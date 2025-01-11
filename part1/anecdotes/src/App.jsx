85a74e320727724d19028d47e279886ec961ea35
ericpedreros <ERIC.PEDREROS@correoaiep.cl>
2025-01-11 08:21:51 -0300

    ejercicio courseinformation usando componentes

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,8 +1,19 @@
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}</p>
+    </div>
+  )
+}
+
 const App = () => {
   console.log('Hello from component')
   return (
     <div>
       <p>Hello World</p>
+      <Hello />
+      <Hello name="George" />
+      <Hello name="Daisy" />
     </div>
   )
 }
--- part1/courseinformation/src/App.jsx
+++ part1/courseinformation/src/App.jsx
@@ -1,7 +1,45 @@
-const App = () => {
+const Header = (props) => {
   return (
     <div>
-      <p>Hello World</p>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part} {props.number}
+      </p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Total of exercises: {props.total}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div className="App">
+      <Header course={course} />
+      <Content part={part1} number={exercises1} />
+      <Content part={part2} number={exercises2} />
+      <Content part={part3} number={exercises3} />
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
